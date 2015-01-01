95132ff5c2c80a983d9cb507d24489a51fa0d54f
Gilles Bouthenot <gilles@mate-vdi>
2015-01-01 15:32:08 +0100

    bashrc-install no longer use relative directory

--- install-bashrc.sh
+++ install-bashrc.sh
@@ -4,8 +4,8 @@ SEARCH=call-dotfiles-gbouthen
 grep $SEARCH $FILE
 if [ $? -eq 0 ] ; then
   echo updating line in $FILE
-  sed -i 's/.*call-dotfiles-gbouthen.*/if [ -d dotfiles-gbouthen ]; then source dotfiles-gbouthen\/bashrc; fi # call-dotfiles-gbouthen #/' $FILE >/dev/null
+  sed -i 's/.*call-dotfiles-gbouthen.*/if [ -d \~\/dotfiles-gbouthen ]; then source \~\/dotfiles-gbouthen\/bashrc; fi # call-dotfiles-gbouthen #/' $FILE >/dev/null
 else
   echo adding line to $FILE
-  echo "if [ -d dotfiles-gbouthen ]; then source dotfiles-gbouthen/bashrc; fi # call-dotfiles-gbouthen #" >>$FILE
+  echo "if [ -d ~/dotfiles-gbouthen ]; then source ~/dotfiles-gbouthen/bashrc; fi # call-dotfiles-gbouthen #" >>$FILE
 fi
