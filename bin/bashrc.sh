d7360df5a6bdf26c3737fbab1a29e27aa13fdd9b
Gilles Bouthenot <misc@atomas.com>
2024-05-02 10:44:09 +0200

    update git config

--- bin/bashrc.sh
+++ bin/bashrc.sh
@@ -123,11 +123,24 @@ done < "$INFILE"
 # --- Git
 #
 if which git >/dev/null 2>&1 ; then
-  git config --global push.default simple "!^$"
-  git config --global gc.autodetach false "!^$"
-  git config --global color.ui auto "!^$"
-  git config --global alias.br branch "!^$"
-  git config --global alias.st status "!^$"
+  git config --global core.autocrlf          "input"
+  git config --global core.safecrlf          "false"
+  git config --global branch.autosetuprebase "always"
+  git config --global push.default           "upstream"
+  git config --global gc.autodetach          "false"
+  git config --global color.ui               "auto"
+
+  git config --global alias.b            "branch -vv"
+  git config --global alias.rb           "branch -rvv"
+  git config --global alias.st           "status"
+#  # https://stackoverflow.com/questions/1838873/visualizing-branch-topology-in-git/34467298#34467298
+  git config --global alias.lg           "lg1"
+  git config --global alias.lg1          "lg1-specific --all"
+  git config --global alias.lg2          "lg2-specific --all"
+  git config --global alias.lg3          "lg3-specific --all"
+  git config --global alias.lg1-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'"
+  git config --global alias.lg2-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'"
+  git config --global alias.lg3-specific "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'"
 fi
 # ---
 
